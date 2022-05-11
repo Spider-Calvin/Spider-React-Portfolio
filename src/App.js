4f822d073bea91ad6caa4bf6e4d9d0601393bfb9
Spider_Calvin <Calvindas7@gmail.com>
2022-05-11 21:22:35 +0530

    File SetUp Done

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
 import "./App.css";
-import Tiitile  from "./Components/Tiitile";
+import { About, Work, Footer, Header, Skills, Testimonial } from "./Container";
+import { Navbar} from './Components'
+import './App.scss'
 
 function App() {
   return (
-   
+    <div className="app">
+      <Navbar/>
+      <Header />
+      <About />
+      <Work />
+      <Skills />
+      <Testimonial />
+      <Footer />
+    </div>
   );
 }
 
---
+++ src/Container/Skills/Skills.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import "./Skills.scss";
+const Skills = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Skills
